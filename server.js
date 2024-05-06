9a839ff2c786fd2cdb38cf0ed6cc8ee35449f379
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-06 14:29:07 +0100

    update

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
-console.log("Hello Nodejs!");
-console.log("Welcome here!");
+// console.log("Hello Nodejs!");
+// console.log("Welcome here!");
 
 
 
-const os = require("os");
+// const os = require("os");
 
-console.log(os.type());
-console.log(os.version());
-console.log(os.homedir());
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+
+const path = require('path');
+
+
+console.log(path.dirname(__filename));
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
