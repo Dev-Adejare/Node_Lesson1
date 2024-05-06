89a04f223c7614388fddb728ddea1fba508ca3cd
Dev-Adejare <sodiqkehinde64@gmail.com>
2024-05-06 14:47:30 +0100

    update

--- server.js
+++ server.js
@@ -17,8 +17,15 @@
 
 
 const path = require('path');
+const {add, div, mult, sub} = require('./math');
 
 
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+console.log(path.parse(__filename));
+console.log(add(3, 5))
+console.log(div(3, 5))
+console.log(mult(3, 5))
+console.log(sub(3, 5))
